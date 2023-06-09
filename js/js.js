545177b43edbc91607197fd27650f486c28caffc
Michaella Moreno <michaella.moreno@gmail.com>
2023-06-09 15:18:40 -0400

    pushing to switch driver

--- js/js.js
+++ js/js.js
@@ -11,12 +11,12 @@
         let movieCard = "";
         for (let i = 0; i < 24; i++) {
             movieCard += `
-                <div class="card col-2" style="">
+                <div class="card col-3 p-4" style="">
                     <img class="card-img-top movie-pic" src="images/${i}.jpg">
                     <div class="card-body row">
                         <h5 class="movie-title">${data[i].title}</h5>
-                        <p class="movie-text">${data[i].info}</p>
-                        <a href="${data[i].link}" target="_blank" class="btn-warning movie-button fw-bold w-50 h-25">IMDb</a>
+                       <div class = "movie-info-container"><p class="movie-text">${data[i].info}</p></div>
+                        <a href="${data[i].link}" target="_blank" class=" btn btn-warning movie-button d-flex justify-content-center align-items-center align-content-center fw-bold w-50 h-25">IMDb</a>
                     </div>
                 </div>
             `;
