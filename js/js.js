af97ae64ad9595efa4070e0d951a03aafc4e30ad
Xavier <xavier.g.medina3@gmail.com>
2023-06-12 09:52:12 -0500

    created search functionality for navbar

--- js/js.js
+++ js/js.js
@@ -17,7 +17,7 @@
         for (let i = 0; i < data.length; i++) {
             movieCard += `
                 <div class="card col-3 p-4 bg-transparent text-light" style="" id="${data[i].id}">
-                    <img class="card-img-top movie-pic" src="images/${i}.jpg" alt="error" onerror="this.src='images/newMovie.jpeg';">
+                    <img class="card-img-top movie-pic" src="images/${data[i].id}.jpg" alt="error" onerror="this.src='images/newMovie.jpeg';">
                     <div class="card-body row">
                         <h5 class="movie-title">${data[i].title}</h5>
                        <div class = "movie-info-container"><p class="movie-text">${data[i].info}</p></div>
@@ -33,7 +33,9 @@
 
     /* ADDS MOVIE FROM FORM TO DATABASE */
 
-    $("#submitButton").on("click", addMovieButton)
+    $("#submitButton").on("click", addMovieButton);
+    $("#deleteButton").on("click", deleteMovieButton);
+    $("#searchForm").on("submit", searchMovie);
 
     function addMovieButton(event){
         event.preventDefault()
@@ -51,6 +53,47 @@
             movieDropList(data)
             location.reload();
         })
+    }
+
+    function deleteMovieButton(event){
+        event.preventDefault();
+        const selectedMovie = $("#movieList").val();
+
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ title: selectedMovie })
+        };
+        console.log(selectedMovie);
+
+
+        fetch(movieURL, options)
+            .then(() => fetch(movieURL))
+            .then(resp => resp.json())
+            .then(data => {
+                movieDropList(data);
+                location.reload();
+            })
+            .catch(error => {
+                console.log("Error deleting movie:", error);
+            });
+    }
+
+    function searchMovie(event){
+        event.preventDefault();
+        const searchTerm = $("#searchInput").val().toLowerCase();
+
+        fetch(movieURL)
+            .then(resp => resp.json())
+            .then(data => {
+                const filteredMovies = data.filter(movie => movie.title.toLowerCase().includes(searchTerm));
+                movieListGen(filteredMovies);
+            })
+            .catch(error => {
+                console.log("Error searching movies:", error);
+            });
     }
 
     // fetch(movieURL, {
@@ -75,4 +118,3 @@
     }
 
 })();
-
